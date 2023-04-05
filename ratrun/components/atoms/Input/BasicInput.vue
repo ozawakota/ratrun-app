12d6c7bb1d700ecdc460d49a586552737063b996
yutamotegi <moyuta0408@gmail.com>
2023-04-05 16:02:52 +0900

    ex: typescriptの導入と使い方

---
+++ ratrun/components/atoms/Input/BasicInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <input :type="type" />
+</template>
+
+<script lang="ts">
+import { PropType } from "vue";
+type InputType = "text" | "number" | "email" | "password";
+export default {
+  props: {
+    type: {
+      type: String as PropType<InputType>, // stringにせず、よりTypeセーフティにする
+      default: "text",
+    },
+  },
+};
+</script>
---
+++ ratrun/components/molecules/Form/AppForm.vue
@@ -0,0 +1,22 @@
+<!-- 使いまわせるようにAppFormとして置いておく -->
+<template>
+  <div>
+    <BasicInput type="text" />
+    <BasicInput type="password" />
+    <BasicButton @onClick="handleOnClick" />
+  </div>
+</template>
+  
+  <script lang="ts">
+import { BasicInput } from "../../atoms/Input";
+import { BasicButton } from "../../atoms/Button";
+export default {
+  components: { BasicInput, BasicButton },
+  setup() {
+    const handleOnClick = (): void => {
+      console.log("click");
+    };
+    return { handleOnClick };
+  },
+};
+</script>
