285bf76a5a53217d2ccae792c50ab114c5a05e17
kouta.ozawa <kouta.ozawa@nadia.bz>
2023-04-05 16:25:47 +0900

    add signeup 静的ページ

---
+++ ratrun/components/molecules/BasicForm/BasicForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form class="signup_form" action="/users/signup">
+    <fieldset class="c-fieldset">
+      <legend>メールアドレス</legend>
+      <div class="form_input">
+        <input type="email" placeholder="taro.tanaka@example.com">
+      </div>
+    </fieldset>
+    <fieldset class="c-fieldset">
+      <legend>パスワード</legend>
+      <div class="form_input">
+        <input type="password" placeholder="パスワード(半角英数6文字以上)">
+      </div>
+    </fieldset>
+    <div class="form-btn signup_form-btn">
+      <button name="button" type="submit" class="c-btn -red">
+        <span>同意して新規登録する(無料)</span>
+      </button>
+    </div>
+  </form>
+</template>
+
+<script setup>
+</script>
+
+<script>
+export default {
+  name: "BasicForm",
+  setup() {},
+};
+</script>
+
+<style lang='scss' scoped>
+.signup{
+  &_form{
+    margin-block-start:30px;
+    &-btn{
+      text-align:center;
+    }
+  }
+}
+</style>
--- ratrun/pages/users/signup.vue
+++ ratrun/pages/users/signup.vue
@@ -1,27 +1,15 @@
+<script setup lang="ts">
+import { Header } from "../../components/organisms/Header";
+import { Footer } from "../../components/organisms/Footer";
+import { BasicForm } from "../../components/molecules/BasicForm";
+</script>
+
 <template>
   <section class="page signup">
     <div class="c-inner">
       <div class="signup_inner">
         <h2 class="p-title">新規会員登録</h2>
-        <form class="signup_form" action="/users/signup">
-          <fieldset class="c-fieldset">
-            <legend>メールアドレス</legend>
-            <div class="form_input">
-              <input type="email" placeholder="taro.tanaka@example.com">
-            </div>
-          </fieldset>
-          <fieldset class="c-fieldset">
-            <legend>パスワード</legend>
-            <div class="form_input">
-              <input type="password" placeholder="パスワード(半角英数6文字以上)">
-            </div>
-          </fieldset>
-          <div class="form-btn signup_form-btn">
-            <button name="button" type="submit" class="c-btn -red">
-              <span>同意して新規登録する(無料)</span>
-            </button>
-          </div>
-        </form>
+        <BasicForm/>
       </div>
       
     </div>
@@ -30,14 +18,12 @@
   </section>
 </template>
 
-<script setup>
-</script>
+
 
 <script >
-import { Header } from "../../components/organisms/Header";
-import { Footer } from "../../components/organisms/Footer";
+
+
 export default {
-  components: { Header, Footer },
   setup() {},
 
 };
@@ -51,12 +37,7 @@ export default {
     max-width:740px;
     margin-inline: auto;
   }
-  &_form{
-    margin-block-start:30px;
-    &-btn{
-      text-align:center;
-    }
-  }
+  
 }
 
 </style>
