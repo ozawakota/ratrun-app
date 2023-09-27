3b6d9477ebf7acb210ed06e464a0564791f979e9
k_ozawa <kouta.ozawa@nadia.bz>
2023-09-27 21:36:26 +0900

    fix loading処理

--- ratrun/pages/index.vue
+++ ratrun/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="top-page">
+    <BasicLoading v-show="isLoading" />
     <p>トップページ</p>
     <ul>
       <li>
@@ -9,7 +10,29 @@
   </div>
 </template>
 
+<script setup lang="ts">
+import { BasicLoading } from "@/components/atoms/Loading/";
+import { ref } from "vue";
 
+const isLoading = ref(false);
+
+(async () => {
+
+  try {
+    isLoading.value = true;
+    // alert(`${isLoading.value} 通信中`);
+  } catch {
+    alert("error");
+  } finally {
+    setTimeout(() => {
+      isLoading.value = false
+      // alert(`${isLoading.value} 通信完了`);
+    }, 2000);
+
+  }
+})();
+
+</script>
 
 <style lang="scss" scoped>
 .top {
